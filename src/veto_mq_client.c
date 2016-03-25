ab6cdf7db47d8c2d00323707aeb7cbdfdc8da128
yanrui <yanrui@192.168.0.9>
2016-03-25 16:23:31 +0800

    first commit veto_mq_client

---
+++ src/veto_mq_client.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : veto_mq_client.c
+ Author      : yanrui
+ Version     :
+ Copyright   : 2015-2050
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <zmq.h>
+#include <zmq_utils.h>
+
+int main(void) {
+
+	 printf ("Connecting to hello world server...\n");
+	    void *context = zmq_ctx_new ();
+	    void *requester = zmq_socket (context, ZMQ_REQ);
+	    zmq_connect (requester, "tcp://localhost:5555");
+
+	    int request_nbr;
+	    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
+	        char buffer [10];
+	        printf ("Sending Hello %d...\n", request_nbr);
+	        zmq_send (requester, "Hello", 5, 0);
+	        zmq_recv (requester, buffer, 10, 0);
+	        printf ("Received World %d\n", request_nbr);
+	    }
+	    zmq_close (requester);
+	    zmq_ctx_destroy (context);
+
+		return 0;
+}
